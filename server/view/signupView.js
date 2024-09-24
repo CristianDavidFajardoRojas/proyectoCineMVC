827eaf695d93298763808bf79fb8b5f54cd80fb9
CristianDavidFajardoRojas <cfajardorojas98@gmail.com>
2024-09-24 17:08:20 -0500

    feat: :sparkles: AutoLog With Cookie

--- server/view/signupView.js
+++ server/view/signupView.js
@@ -1,9 +1,9 @@
 const layout = require('express').Router();
 const { join } = require('path');
 const cookieParser =  require('cookie-parser');
-//const { authLogged } =  require('../middleware/authCookies');
+const { authLogged } =  require('../middleware/authCookies');
 
-layout.get("/", cookieParser(), (req, res)=>{ //authLogged
+layout.get("/", cookieParser(), authLogged, (req, res)=>{ 
     res.sendFile(join(req.__dirname, process.env.EXPRESS_STATIC, '/view/signUp.html'));
 })
 
