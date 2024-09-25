03b2f200482ac982245bba58ca2a26ee360a3819
CristianDavidFajardoRojas <cfajardorojas98@gmail.com>
2024-09-25 17:39:12 -0500

    feat: :sparkles: Carrousel Peliculas Disponibles

--- src/js/mainPage.js
+++ src/js/mainPage.js
@@ -2,16 +2,28 @@ let uri = `${location.href}/v1`;
 let movie_list = document.querySelector('.movie-list');
 
 const showData = (data) => {
-    console.log(data, "ASDDASDSADSD");
-
     let plantilla = '';
 
     data.forEach( movie => {
-        if(new Date(movie.fecha_estreno) > new Date() && new Date(movie.fecha_retiro) > new Date()){
-            console.log(movie)
-        }
-    })
+        const fechaEstreno = new Date(movie.fecha_estreno);
+        const fechaRetiro = new Date(movie.fecha_retiro);
+        const fechaActual = new Date()
 
+        if (fechaEstreno < fechaActual && fechaRetiro > fechaActual) plantilla += /*html*/`
+
+        <div>
+            <img src="${movie.imagen}" alt="Movie 1" class="movie-poster">
+            <p class="movie-title">${movie.titulo}</p>
+        </div>
+
+        `
+
+
+        // if(fechaEstreno < fechaActual && fechaRetiro > fechaActual){
+        //     console.log("INSANOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO", movie)
+        // }else console.log("ADF")
+    })
+    console.log(plantilla)
     return plantilla;
 }
 
@@ -21,9 +33,9 @@ addEventListener('DOMContentLoaded', async()=>{
     let peticion = await fetch(uri);
     let res = await peticion.json();
     if(res.data){
-        //if(res.status == 200) myTbody.innerHTML = await showData(res.data);
+        if(res.status == 200) movie_list.innerHTML = await showData(res.data);
         await showData(res.data);
     }else{
-        //myTbody.innerHTML = await showData();
+        movie_list.innerHTML = await showData();
         await showData();
 }})
