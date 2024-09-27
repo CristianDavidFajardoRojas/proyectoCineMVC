16cea6b066f84ed3391f76f34f6137a60b084592
CristianDavidFajardoRojas <cfajardorojas98@gmail.com>
2024-09-27 14:29:02 -0500

    feat: :sparkles: Seleccion de cine.
    Valida la seleccion de cine y si la pelicula esta disponible, si cumple los requesitos, puede avanzar al booking.

--- src/js/movieDetail.js
+++ src/js/movieDetail.js
@@ -32,18 +32,19 @@ const showData = (data) => {
     </div>
     <div class="movie-poster" style="background-image: url('${data.imagen}');"></div>
     <div class="movie-title">${data.titulo}</div>
-    <div class="movie-info">${data.sinopsis} • ${Math.floor(data.duracion / 60)}h ${data.duracion % 60}m</div>
+    <div class="movie-info">${data.sinopsis} • ${Math.floor(data.duracion / 60)}h ${(data.duracion % 60)!=0 ? ((data.duracion % 60) + 'm') : ''}</div>
     <div class="cast">
         ${plantillaCast}
     </div>
     <div class="cinema-option">
-        <div class="cinema-name">Atrium Cinemas</div>
-        <div class="cinema-info">Karachi • 5 km</div>
+        <article>
+            <div class="cinema-name">CineCampus</div>
+            <div class="cinema-info">Santander, Zona Franca.</div>
+            <img src="https://yt3.googleusercontent.com/s0BLxzTiC8JcXrFSwbe7Ef5eIP8DHu33a_jRqjtuhZZiEuc5P4DhJ3hreKWt4CIY8vkf3t5iL2U=s160-c-k-c0x00ffffff-no-rj" id="ImagenCineCampus">
+        </article>
     </div>
-    <div class="cinema-option">
-        <div class="cinema-name">Nueplex</div>
-        <div class="cinema-info">Karachi • 5.7km • DHA Phase 8</div>
-    </div>
+
+    
     <button class="book-button">Book Now</button>
 
         `
@@ -63,6 +64,52 @@ addEventListener('DOMContentLoaded', async()=>{
     let back_button = document.querySelector('.back-button');
     back_button.addEventListener('click', () => {
         window.location.href = "/mainPage";
+    })
+
+    let book_button = document.querySelector('.book-button');
+    let cinema_option = document.querySelectorAll('.cinema-option > article');
+    let cine_selected = false;
+    const fechaEstreno = new Date(res.data[0].fecha_estreno);
+    const fechaRetiro = new Date(res.data[0].fecha_retiro);
+    const fechaActual = new Date()
+
+    if (fechaEstreno > fechaActual) {
+        book_button.disabled = true;
+        book_button.innerHTML = "Coming Soon..."
+    }
+
+    cinema_option.forEach(cineOption => {
+
+        cineOption.addEventListener('click', () => {
+            if (cineOption.id != 'cine-selected') {
+                cinema_option.forEach(cineOptionId => {
+                    cineOptionId.id = '';
+                })
+                cineOption.id = "cine-selected"
+                cine_selected = true;
+                book_Button_Validation();
+            } else {
+                cineOption.id = "";
+                cine_selected = false;
+                book_Button_Validation()
+            }
+        })
+
+    });
+
+    const book_Button_Validation = () => {
+
+    if(book_button.disabled != true && cine_selected != false) {
+        book_button.id = "book-avaible";
+    } else {
+        book_button.id = "";
+    }};
+
+    book_button.addEventListener('click', () => {
+        if(book_button.id == "book-avaible"){
+            alert("HOLAAA")
+            //window.location.href = `/bookingPage?id=${id}`;
+        }
     })
 
 })
