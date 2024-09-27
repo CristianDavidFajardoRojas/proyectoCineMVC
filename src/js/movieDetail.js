6d33ccdcf055c7a40bfc0b9f87b60466c1b1d0d5
CristianDavidFajardoRojas <cfajardorojas98@gmail.com>
2024-09-27 07:29:13 -0500

    feat: :sparkles: Show Data Detail /detailMovie

--- src/js/movieDetail.js
+++ src/js/movieDetail.js
@@ -1,24 +1,54 @@
+let body = document.querySelector('body');
 
 const id = new URL(window.location.href).searchParams.get("id");
 let uri = `${location.origin}${location.pathname}/v1/${id}`;
 
 const showData = (data) => {
-    let plantilla = '';
 
-    data.forEach( movie => {
-        const fechaEstreno = new Date(movie.fecha_estreno);
-        const fechaRetiro = new Date(movie.fecha_retiro);
-        const fechaActual = new Date()
+    console.log(data);
 
-        if (fechaEstreno < fechaActual && fechaRetiro > fechaActual) plantilla += /*html*/`
+    let plantillaCast = '';
 
-        <div class="ChooseMovie" id="${movie._id}">
-            <img src="${movie.imagen}" alt="Movie 1" class="movie-poster">
-            <p class="movie-title">${movie.titulo}</p>
+    data.cast.forEach(cast => {
+        plantillaCast += /*html*/`
+
+        <div class="cast-member">
+            <img src="${cast.imagen}" alt="" class="cast-image">
+            <div class="cast-name">${cast.nombre}</div>
+            <div class="cast-name">${cast.rol}</div>
         </div>
 
         `
     })
+    
+    let plantilla = '';
+  
+        plantilla += /*html*/`
+
+        <div class="header">
+        <button class="back-button">&#8592;</button>
+        <h1>Cinema Selection</h1>
+        <button class="menu-button">&#8942;</button>
+    </div>
+    <div class="movie-poster" style="background-image: url('${data.imagen}');"></div>
+    <div class="movie-title">${data.titulo}</div>
+    <div class="movie-info">${data.sinopsis} • ${Math.floor(data.duracion / 60)}h ${data.duracion % 60}m</div>
+    <div class="cast">
+        ${plantillaCast}
+    </div>
+    <div class="cinema-option">
+        <div class="cinema-name">Atrium Cinemas</div>
+        <div class="cinema-info">Karachi • 5 km</div>
+    </div>
+    <div class="cinema-option">
+        <div class="cinema-name">Nueplex</div>
+        <div class="cinema-info">Karachi • 5.7km • DHA Phase 8</div>
+    </div>
+    <button class="book-button">Book Now</button>
+
+        `
+        
+    body.innerHTML = plantilla
 
     return plantilla;
 }
@@ -28,5 +58,11 @@ addEventListener('DOMContentLoaded', async()=>{
     console.log(uri)
     let peticion = await fetch(uri);
     let res = await peticion.json();
-    console.log(res)
+    showData(res.data[0])
+    
+    let back_button = document.querySelector('.back-button');
+    back_button.addEventListener('click', () => {
+        window.location.href = "/mainPage";
+    })
+
 })
