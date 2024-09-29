42e08252027b81cb06341f0dc2cd4755763d1f03
CristianDavidFajardoRojas <cfajardorojas98@gmail.com>
2024-09-29 05:12:37 -0500

    feat: :sparkles: Cookie Verify
    Si la cookie no existe, sera redireccionado al openning para registrarse o para logear.

--- server/view/mainPage.js
+++ server/view/mainPage.js
@@ -1,8 +1,9 @@
 const layout = require('express').Router();
 const { join } = require('path');
 const cookieParser =  require('cookie-parser');
+const { authMainPage } =  require('../middleware/authMain');
 
-layout.get("/", cookieParser(), (req, res)=>{ 
+layout.get("/", cookieParser(), authMainPage, (req, res)=>{ 
     res.sendFile(join(req.__dirname, process.env.EXPRESS_STATIC, '/view/mainPage.html'));
 })
 
